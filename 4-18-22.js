d6d5962dd5c4cd00bcb7bf760c34bac18b277bed
Wendi (Isaac) Li <95248250+THEWENDI@users.noreply.github.com>
2022-04-18 23:28:34 -0700

    Update 4-18-22.js

--- 4-18-22.js
+++ 4-18-22.js
@@ -18,17 +18,17 @@ function isPresent2d(arr2d, value){
 
 console.log(isPresent2d(arr2d,0))
 
-// //flatten the array
+//flatten the array
 
-// function flatten(arr2d){
-//     var flat = [];
-//     for(i = 0; i < arr2d.length;i++){
-//         for(x = 0; x < arr2d[i].length;x++){
-//             flat.push(arr2d[i][x]);
-//         }
-//     }
-//     return flat;
-// }
+function flatten(arr2d){
+    var flat = [];
+    for(i = 0; i < arr2d.length;i++){
+        for(x = 0; x < arr2d[i].length;x++){
+            flat.push(arr2d[i][x]);
+        }
+    }
+    return flat;
+}
 
 // var result = flatten([[2, 5, 8],[3, 6, 1],[5, 7, 7]]);
 // console.log(result);

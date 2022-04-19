701a978a731ecacca0b5369a00322cbd422afcbd
Wendi (Isaac) Li <95248250+THEWENDI@users.noreply.github.com>
2022-04-18 23:27:27 -0700

    Update 4-18-22.js

--- 4-18-22.js
+++ 4-18-22.js
@@ -2,19 +2,19 @@ var arr2d = [ [2, 5, 8],
               [3, 6, 1],
               [5, 7, 7] ];
 
-// function isPresent2d(arr2d, value){
-//     for(var i = 0; i < arr2d.length; i++)
-//     {
-//         for(var x = 0; x < arr2d[i].length; x++)
-//         {
-//             if(arr2d[i][x] == value)
-//             {
-//                 return true;
-//             }
-//         }
-//     }
-//     return false
-// }
+function isPresent2d(arr2d, value){
+    for(var i = 0; i < arr2d.length; i++)
+    {
+        for(var x = 0; x < arr2d[i].length; x++)
+        {
+            if(arr2d[i][x] == value)
+            {
+                return true;
+            }
+        }
+    }
+    return false
+}
 
 console.log(isPresent2d(arr2d,0))
 

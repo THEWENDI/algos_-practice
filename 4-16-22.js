5dc6bf6a6a6779e5ccfe2db2f46fdd283d775219
Wendi Li <issacbbb@hotmaill.com>
2022-04-18 23:22:31 -0700

    initial commit

--- 4-16-22.js
+++ 4-16-22.js
@@ -24,29 +24,29 @@
 // console.log("The player rolled: " + playerRoll);
 
 function random(){
-    var lifesAnswers = [
-        "It is certain.",
-        "It is decidedly so.",
-        "Without a doubt.",
-        "Yes – definitely.",
-        "You may rely on it.",
-        "As I see it, yes.",
-        "Most likely.",
-        "Outlook good.",
-        "Yes.",
-        "Signs point to yes.",
-        "Reply hazy, try again.",
-        "Ask again later.",
-        "Better not tell you now.",
-        "Cannot predict now.",
-        "Concentrate and ask again.",
-        "Don't count on it.",
-        "My reply is no.",
-        "My sources say no.",
-        "Outlook not so good.",
-        "Very doubtful."
-    ];
-    var roll = Math.floor(Math.random()*(lifesAnswers.length - 0 + 1));
-        return lifesAnswers[roll];
-    }
-    console.log(random())
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+var roll = Math.floor(Math.random()*(lifesAnswers.length - 0 + 1));
+    return lifesAnswers[roll];
+}
+console.log(random())

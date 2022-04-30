b5232806fc42775d8fd8f955adcebbffaf97a73c
Wendi (Isaac) Li <95248250+THEWENDI@users.noreply.github.com>
2022-04-29 20:27:13 -0700

    Update 4-29-22.js

--- 4-29-22.js
+++ 4-29-22.js
@@ -25,10 +25,10 @@ function isValidSubsequence(array, sequence) {
 // }
 
 
-const array = [5, 1, 22, 25, 6, -1, 8, 10]
-const sequence = [1, 6, -1, 10]
+// const array = [5, 1, 22, 25, 6, -1, 8, 10]
+// const sequence = [1, 6, -1, 10]
 
-console.log(isValidSubsequence(array, sequence));
+// console.log(isValidSubsequence(array, sequence));
 
 
 function sortedSquaredArray(array) {

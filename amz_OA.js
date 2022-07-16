7b776d1d143edd0cd37ac842a980c0c467deb0a4
Wendi (Isaac) Li <95248250+THEWENDI@users.noreply.github.com>
2022-07-15 22:30:11 -0700

    Update amz_OA.js

--- amz_OA.js
+++ amz_OA.js
@@ -1,30 +1,30 @@
 //////Two Sum - Unique Pairs///////
 ///////////////////////////////////
 
-Write a function that takes a list of numbers and a target number, and then returns the number of unique pairs that add up to the target number.
+// Write a function that takes a list of numbers and a target number, and then returns the number of unique pairs that add up to the target number.
 
-[X, Y] and [Y, X] are considered the same pair, and not unique.
+// [X, Y] and [Y, X] are considered the same pair, and not unique.
 
-Examples
-Example 1:
-Input: [1, 1, 2, 45, 46, 46], target = 47
-Output: 2
-Explanation:
-1 + 46 = 47
+// Examples
+// Example 1:
+// Input: [1, 1, 2, 45, 46, 46], target = 47
+// Output: 2
+// Explanation:
+// 1 + 46 = 47
 
-2 + 45 = 47
+// 2 + 45 = 47
 
-Example 2:
-Input: [1, 1], target = 2
-Output: 1
-Explanation:
-1 + 1 = 2
+// Example 2:
+// Input: [1, 1], target = 2
+// Output: 1
+// Explanation:
+// 1 + 1 = 2
 
-Example 3:
-Input: [1, 5, 1, 5], target = 6
-Output: 1
-Explanation:
-[1, 5] and [5, 1] are considered the same, therefore there is only one unique pair that adds up to 6.
+// Example 3:
+// Input: [1, 5, 1, 5], target = 6
+// Output: 1
+// Explanation:
+// [1, 5] and [5, 1] are considered the same, therefore there is only one unique pair that adds up to 6.
 
 const array = [1, 1, 2, 45, 46, 46];
 const targetSum = 47;

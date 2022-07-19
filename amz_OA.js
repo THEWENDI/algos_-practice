845ad804087390ae2fb43f1b766310b69309b76b
Wendi (Isaac) Li <95248250+THEWENDI@users.noreply.github.com>
2022-07-18 21:14:57 -0700

    Update amz_OA.js

--- amz_OA.js
+++ amz_OA.js
@@ -310,20 +310,20 @@ console.log(slowestKey(releaseTimes, keysPressed));
 // Space complexity : O(n) || O(k)
 
 
-/////Top K Frequent Words////////
-/////////////////////////////////
+// /////Top K Frequent Words////////
+// /////////////////////////////////
 
 
-Given an array of strings words and an integer k, return the k most frequent strings.
+// Given an array of strings words and an integer k, return the k most frequent strings.
 
-Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+// Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
 
-Example 1:
+// Example 1:
 
-Input: words = ["i","love","leetcode","i","love","coding"], k = 2
-Output: ["i","love"]
-Explanation: "i" and "love" are the two most frequent words.
-Note that "i" comes before "love" due to a lower alphabetical order.
+// Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+// Output: ["i","love"]
+// Explanation: "i" and "love" are the two most frequent words.
+// Note that "i" comes before "love" due to a lower alphabetical order.
 // Example 2:
 
 // Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4

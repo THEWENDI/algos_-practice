4b0b657c13cd7d50b9c2a208e02ae122e9080eb7
Wendi (Isaac) Li <95248250+THEWENDI@users.noreply.github.com>
2022-07-28 22:40:33 -0700

    Update amz_OA.js

--- amz_OA.js
+++ amz_OA.js
@@ -717,3 +717,29 @@ var topKFrequent = function(nums, k) {
         }
     }
 };
+
+var findMin = function(nums) {
+    if (nums.length === 1){
+        return nums[0];
+    }
+    let l = 0;
+    let r = nums.length - 1;
+    if (nums[r] > nums[l]){
+        return nums[0];
+    }
+    
+    while ( l <= r ){
+        let mid = Math.floor((l + r)/2)
+        if (nums[mid] < nums[mid - 1]){
+            return nums[mid]
+        }
+        if (nums[mid + 1] < nums[mid]){
+            return nums[mid + 1]
+        }
+        if(nums[l] < nums[mid]){
+            l = mid + 1;
+        }else{
+            r = mid - 1;
+        }
+    }
+};
